c2c57de5ee2e277bc4531181c23df36845812fd1
Ole Wieners <olewieners@yahoo.com>
2026-02-18 15:19:33 +0100

    Add playlists table and API connection
    This adds the titular table and everything necessary to
    show all playlists.
    
    On its own not of much use, apart from looking at the playlist
    names.
    
    Creating, editing, deleting etc. will be added in a later
    commit.
    
    Needs https://github.com/owi92/opencast/tree/playlist-admin-ui-api to work.

---
+++ src/configs/tableConfigs/playlistsTableMap.ts
@@ -0,0 +1,10 @@
+import PlaylistCreatorCell from "../../components/events/partials/PlaylistCreatorCell";
+import PlaylistUpdatedCell from "../../components/events/partials/PlaylistUpdatedCell";
+
+/**
+ * This map contains the mapping between the template strings and the corresponding react component.
+ */
+export const playlistsTemplateMap = {
+  PlaylistCreatorCell: PlaylistCreatorCell,
+  PlaylistUpdatedCell: PlaylistUpdatedCell,
+};
